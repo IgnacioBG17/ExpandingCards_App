ee2cf134727a1f942fba73d56c3e22b8767232d4
Angel Bustamante <bustamanteangel532@gmail.com>
2025-07-16 11:55:20 -0600

    se comenta el codigo para explicacion

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// Selecciona todos los elementos del DOM con la clase 'Panel'
 const panels = document.querySelectorAll('.panel')
 
+// Itera sobre cada panel y le agrega un listener para el evento 'Click'
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
+        // Cuando se hace click en un panel, se eliminan todas las clases 'active' de todos los paneles
         removeActiveClasses()
+        // Luego, se agrega la clase 'active' solo al panel que fue clickeado
         panel.classList.add('active')
     })
 })
 
-
+// Funcion que elimina la clase 'active' de todos los paneles
 function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
